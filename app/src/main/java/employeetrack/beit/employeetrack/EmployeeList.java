593672a8c471abc10a2e3c86d662842f549e33d8
harshadfriend <harshad.friend@gmail.com>
2018-04-04 16:32:19 +0530

    commit

--- app/src/main/java/employeetrack/beit/employeetrack/EmployeeList.java
+++ app/src/main/java/employeetrack/beit/employeetrack/EmployeeList.java
@@ -9,20 +9,56 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class EmployeeList extends AppCompatActivity {
 
     ListView lvEmployee;
     String emplist[]={"Emp1","Emp2","Emp3","Emp4","Emp5","Emp6","Emp7","Emp8","Emp9"};
     ArrayAdapter<String> adp;
+    Firebase firebase;
+    String dburl="https://employeetracking-1caec.firebaseio.com/";
+    DatabaseReference dbRef;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_list);
 
+        Firebase.setAndroidContext(this);
+        firebase=new Firebase(dburl);
+
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
         lvEmployee=(ListView)findViewById(R.id.lvEmployee);
         adp=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        adp.setNotifyOnChange(true);
         lvEmployee.setAdapter(adp);
+
+        Query q=dbRef.child("employee");
+        q.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot data:dataSnapshot.getChildren()){
+                    fbase f=data.getValue(fbase.class);
+                    adp.add(f.getName()+"\n"+f.getAddress()+"\n"+f.getMobile());
+                }
+                lvEmployee.setAdapter(adp);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         lvEmployee.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
--- app/src/main/java/employeetrack/beit/employeetrack/VehicleList.java
+++ app/src/main/java/employeetrack/beit/employeetrack/VehicleList.java
@@ -8,20 +8,55 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class VehicleList extends AppCompatActivity {
 
     ListView lvVehicle;
     String vehiclelist[]={"Vehicle1","Vehicle2","Vehicle3","Vehicle4","Vehicle5","Vehicle6","Vehicle7","Vehicle8","Vehicle9"};
     ArrayAdapter<String> adp;
+    Firebase firebase;
+    String dburl="https://employeetracking-1caec.firebaseio.com/";
+    DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_list);
 
+        Firebase.setAndroidContext(this);
+        firebase=new Firebase(dburl);
+
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
+
         lvVehicle=(ListView)findViewById(R.id.lvVehicle);
         adp=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        adp.setNotifyOnChange(true);
         lvVehicle.setAdapter(adp);
+
+        Query q=dbRef.child("vehicle");
+        q.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot data:dataSnapshot.getChildren()){
+                    fbase f=data.getValue(fbase.class);
+                    adp.add(f.getName()+"\n"+f.getAddress()+"\n"+f.getMobile());
+                }
+                lvVehicle.setAdapter(adp);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         lvVehicle.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
